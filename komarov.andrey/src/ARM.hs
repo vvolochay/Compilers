25162a3f46206214542ba1ffdda5f030e36c08ed
Andrey Komarov <taukus@gmail.com>
2015-03-18 04:01:56 +0300

    ARM AST started

---
+++ komarov.andrey/src/ARM.hs
@@ -0,0 +1,64 @@
+module ARM (
+
+  ) where
+
+import Data.Int
+   
+data Register = R0 | R1 | R2 | R3
+              | R4 | R5 | R6 | R7
+              | R8 | R9 | R10 | R11
+              | R12 | R13 | R14 | R15
+  deriving (Eq, Ord, Show)
+
+bp, sp, lr, pc :: Register
+bp = R11
+sp = R13
+lr = R14
+pc = R15
+
+data Cond = Eq -- Z set
+          | Ne -- Z clear
+          | Cs -- C set
+          | Cc -- C clear
+          | Mi -- N set
+          | Pl -- N clear
+          | Vs -- V set
+          | Vc -- V clear
+          | Hi -- Cs & Zc
+          | Ls -- Cc | Zs
+          | Ge -- (Ns & Vs) | (Nc & Vc)
+          | Lt -- (Ns & Vc) | (Nc & Vs)
+          | Gt -- Zc & Ge
+          | Le -- Zs & Lt
+          | Al -- always
+          | Nv -- reserved
+
+data SetFlags = Update | Ignore
+
+-- TODO добавить barrel shifter
+data Operand2 = Reg Register | Imm Int32
+
+data Width = Word | HalfWord | Byte
+
+data OpCode
+  = ADD SetFlags Register Register Operand2
+  | SUB SetFlags Register Register Operand2
+  | RSB SetFlags Register Register Operand2
+  | MUL SetFlags Register Register Operand2
+  | B Int32
+  | BL Int32
+  | CMP Register Operand2
+  | CMN Register Operand2
+  | TST Register Operand2
+  | TEQ Register Operand2 -- xor
+  | AND SetFlags Register Register Operand2
+  | EOR SetFlags Register Register Operand2
+  | ORR SetFlags Register Register Operand2
+  | BIC SetFlags Register Register Operand2 -- and not
+  | MOV SetFlags Register Operand2
+  | MVN SetFlags Register Operand2
+  | LDR' Register Int32 -- cheats
+  | LDR Width Register Register Operand2
+  | STR Width Register Register Operand2
+  | SWI
+
--- komarov.andrey/src/TypedAST.hs
+++ komarov.andrey/src/TypedAST.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE GADTs, KindSignatures, DataKinds, PolyKinds #-}
 {-# LANGUAGE TypeOperators, RankNTypes, ImpredicativeTypes #-}
 module TypedAST (
 
   ) where
 
+import Control.Monad.Reader
+import Control.Monad.Error
+import Control.Applicative
+import Data.List (find)
+
 import qualified Data.Map as M
+
+import qualified AST
 
 type Id = String
 
@@ -13,51 +21,17 @@ data Type = TBool
           | TString
           | TVoid
 
-data LiftType :: Type -> * where
-  LiftBool :: LiftType TBool
-  LiftInt :: LiftType TInt
-  LiftString :: LiftType TString
-  LiftVoid :: LiftType TVoid
+type Env = [(Id, Type)]
 
-type Env = Id -> Type
-type Scope = [(Id, Type)]
+newtype Typecheck a = Typecheck { unTypecheck :: ErrorT String (Reader Env) a }
+                    deriving (Functor, Applicative, Monad, MonadReader Env, MonadError String)
 
-data TStatement :: Type -> * where
-  TBlock :: TStatement t -> [TStatement t] -> TStatement t
-  TEmpty :: TStatement t
-  TVariableDeclaration :: Type -> [Id] -> TStatement t
-  TAssignment :: Id -> TExpr t' -> TStatement t
-  TRawExpression :: TExpr t' -> TStatement t
-  TIfThenElse :: TExpr TBool -> TStatement t -> TStatement t ->
-                 TStatement t
-  TWhile :: TExpr TBool -> TStatement t -> TStatement t
-  TReturn :: TExpr t -> TStatement t
-  
+getType :: Id -> Typecheck Type
+getType name = do
+  env <- ask
+  case find ((== name) . fst) env of
+   Nothing -> throwError $ "No such var in env: " ++ name
+   Just t -> return $ snd t
 
-data TFunction :: [Type] -> Type -> * where
-  TFunction :: Types argTypes -> Scope -> TStatement ret ->
-               TFunction argTypes ret
-
-data TList (f :: k -> *) :: [k] -> * where
-  Nil :: TList f '[]
-  Cons :: f t -> TList f tx -> TList f (t ': tx)
-
-type TExprs = TList TExpr
-type Types = TList LiftType
-
-data TExpr :: Type -> * where
-  TEInt :: Int -> TExpr TInt
-  TEBool :: Bool -> TExpr TBool
-  TEAddInt :: TExpr TInt -> TExpr TInt -> TExpr TInt
-  TESub :: TExpr TInt -> TExpr TInt -> TExpr TInt
-  TEMul :: TExpr TInt -> TExpr TInt -> TExpr TInt
-  TELess :: TExpr TInt -> TExpr TInt -> TExpr TBool
-  TEGreater :: TExpr TInt -> TExpr TInt -> TExpr TBool
-  TELessEq :: TExpr TInt -> TExpr TInt -> TExpr TBool
-  TEGreaterEq :: TExpr TInt -> TExpr TInt -> TExpr TBool
-  TEEqual :: TExpr t -> TExpr t -> TExpr TBool
-  TENotEqual :: TExpr t -> TExpr t -> TExpr TBool
-  TEAnd :: TExpr TBool -> TExpr TBool -> TExpr TBool
-  TEOr :: TExpr TBool -> TExpr TBool -> TExpr TBool
-  TECall :: TFunction args ret -> TExprs args -> TExpr res
-
+typecheck :: AST.Statement -> Typecheck ()
+typecheck (AST.Block []) = _
