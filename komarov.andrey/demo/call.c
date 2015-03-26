f439276f7683c0266c12e0f2c56b0e21ba678402
Andrey Komarov <taukus@gmail.com>
2015-03-26 22:26:11 +0300

    demo added

---
+++ komarov.andrey/demo/call.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int f();
+
+int main()
+{
+    printf("%d\n", f());
+}
+
---
+++ komarov.andrey/demo/prime.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int isPrime(int n);
+int numPrimes(int from, int to);
+
+int main()
+{
+    int i;
+    for (i = 2; i < 20; i++)
+    {
+        printf("%d is %s\n", i, isPrime(i) ? "prime" : "not prime");
+    }
+    printf("%d primes between 100 and 2000\n", numPrimes(100, 5000));
+}
+
