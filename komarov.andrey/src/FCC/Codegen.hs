c357694ad911b9a64eb7da097da77f2bf37b6230
Andrey Komarov <taukus@gmail.com>
2015-05-18 23:42:31 +0300

    codegen for expr finished

--- komarov.andrey/src/FCC/Codegen.hs
+++ komarov.andrey/src/FCC/Codegen.hs
@@ -13,13 +13,14 @@ import Control.Monad.State
 
 impossible = error "Internal compiler error. Please submit a bug-report."
 
-data Binding = Local Int | Global String
+data Binding = Local Int | Global String | Arg Int
                            deriving (Eq, Ord, Show, Read)
 
-data CodegenState = CodegenState { offset :: Int, counter :: Int }
+data CodegenState = CodegenState { offset :: Int, counter :: Int, maxOffset :: Int }
 
 modifyOffset :: MonadState CodegenState m => (Int -> Int) -> m ()
-modifyOffset f = modify $ \s@(CodegenState{offset = o}) -> s{offset = f o}
+modifyOffset f = modify $ \s@(CodegenState{maxOffset = m, offset = o})
+                          -> s{maxOffset = m `max` o, offset = f o}
 
 newtype Codegen a = Codegen {
   runCodegen :: State CodegenState a
@@ -41,33 +42,53 @@ codegen :: Program a -> [String]
 codegen = _
 
 compileE :: Expr Binding -> Codegen [String]
-compileE (Var (Local off)) = return ["ldr r0, [fp, #" ++ show off ++"]", "push r0"]
+compileE (Var (Local off)) = return ["ldr r0, [fp, #-" ++ show (off * 4) ++"]", "push r0"]
 compileE (Var (Global name)) = return ["ldr r0, " ++ show name, "push r0"]
+compileE (Var (Arg arg)) = return ["ldr r0, [fp, #" ++ show (arg * 4 + 8) ++ "]", "push r0"]
 compileE (Lit i) = return ["push =" ++ show i]
 compileE (LitBool True) = return ["push #1\t\t@ true"]
 compileE (LitBool False) = return ["push #0\t\t@ false"]
 compileE (Lam t s) = do
   v <- freshVar
   off <- gets offset
-  modifyOffset (+4) -- TODO посчитать максимальный offset
+  modifyOffset (+1)
   code <- compileE $ instantiate1 (Var (Local off)) s
-  modifyOffset (-4)
+  modifyOffset (-1)
   return code
 compileE Empty = return []
 compileE (Pop e) = do
   code <- compileE e
   return $ code ++ ["pop r0"]
 compileE (Seq e1 e2) = (++) <$> compileE e1 <*> compileE e2
-compileE (Call f args) = _
+compileE (Call (Var (Global fname)) args) = do
+  compiledArgs <- reverse <$> concat <$> mapM compileE args
+  return $ compiledArgs ++ ["bl " ++ fname]
+compileE (Call _ _) = impossible
 compileE (Eq _ _) = impossible
-compileE (While cond body) = _
-compileE (If cont thn els) = _
+compileE (While cond body) = do
+  begin <- freshLabel
+  end <- freshLabel
+  cond' <- compileE cond
+  body' <- compileE body
+  return $ [begin ++ ": @ while"] ++ cond' ++ ["pop r0", "tst r0, r0", "bz " ++ end] ++ body' ++ [end ++ ": @ endwhile"]
+compileE (If cond thn els) = do
+  elseLabel <- freshLabel
+  endIfLabel <- freshLabel
+  cond' <- compileE cond
+  thn' <- compileE thn
+  els' <- compileE els
+  return $ cond' ++ ["pop r0", "tst r0, r0", "bz " ++ elseLabel]
+    ++ thn' ++ ["b " ++ endIfLabel, elseLabel ++ ": @ else:"]
+    ++ els' ++ [endIfLabel ++ ": @ endif"]
 compileE (Assign (Var (Local off)) src) = do
   code <- compileE src
-  return $ code ++ ["pop r0", "str r0, [fp, #" ++ show (off * 4) ++ "]", "push r0"]
+  return $ code ++ ["pop r0", "str r0, [fp, #-" ++ show (off * 4) ++ "]", "push r0"]
 compileE (Assign (Var (Global name)) src) = do
   code <- compileE src
   return $ code ++ ["pop r0", "ldr r1, " ++ show name, "str r0, [r1]", "push r0"]
+compileE (Assign (Var (Arg arg)) src) = do
+  code <- compileE src
+  return $ code ++ ["pop r0", "str r0, [fp, #" ++ show (arg * 4 + 8) ++ "]", "push r0"]
 compileE (Assign (Array a i) src) = do
   codea <- compileE a
   codei <- compileE i
